5e06dd319a0594b3f399da3ea12d0591cd497f38
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-02-11 23:16:10 +0900

    :sparkles:first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./App.module.css";
+
+import { FaSignInAlt } from "react-icons/fa";
+import TaskList from "./features/task/TaskList";
+
+function App() {
+  const Logout = () => {
+    localStorage.removeItem("localJWT");
+    window.location.href = "/";
+  };
+  return (
+    <div className={styles.containerTasks}>
+      <div className={styles.appTasks}>
+        <button onClick={Logout} className={styles.signBtn}>
+          <FaSignInAlt />
+        </button>
+        <TaskList />
+      </div>
+      <div className={styles.appDetails}></div>
+    </div>
+  );
+}
+
+export default App;
